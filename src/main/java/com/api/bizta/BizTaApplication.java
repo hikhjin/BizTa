fb2f05f94709bd3d024ec7477151516432fe36c7
김현진 <kimhyeonjin@gimhyeonjin-ui-MacBookPro.local>
2023-04-04 17:54:48 +0900

    test

---
+++ src/main/java/com/api/bizta/BizTaApplication.java
@@ -0,0 +1,13 @@
+package com.api.bizta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizTaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BizTaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/bizta/BizTaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.bizta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizTaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
