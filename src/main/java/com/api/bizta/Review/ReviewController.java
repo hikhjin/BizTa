592177d52b5467b2f1630598071d95ac13facc91
ohhyungsuh <ohgudtj_3@naver.com>
2023-04-10 15:20:11 +0900

    [MODIFY] RequestParam 추가, 정렬 기능 추가

--- src/main/java/com/api/bizta/Review/ReviewController.java
+++ src/main/java/com/api/bizta/Review/ReviewController.java
@@ -6,6 +6,8 @@ import com.api.bizta.config.BaseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -22,12 +24,40 @@ public class ReviewController {
 
     @ResponseBody
     @GetMapping("/{placeIdx}/reviews")
-    public BaseResponse<List<GetReviewInfos>> getReviewInfos(@PathVariable("placeIdx") int placeIdx){
+    public BaseResponse<List<GetReviewInfos>> getReviewInfos(@PathVariable("placeIdx") int placeIdx, @RequestParam(defaultValue = "latest") String sort, @RequestParam(defaultValue = "descending") String order){
         try{
             List<GetReviewInfos> reviewInfos = reviewProvider.getReviewInfos(placeIdx);
+
+            if(sort.equals("latest") && order.equals("ascending")){ // 오래된순
+                return new BaseResponse<>(reviewInfos);
+            }else if(sort.equals("popularity") && order.equals("descending")){ // 인기순
+                Collections.sort(reviewInfos, (o1, o2) -> {
+                    if(o1.getRating() > o2.getRating()){
+                        return -1;
+                    }else{
+                        return 1;
+                    }
+                });
+
+                return new BaseResponse<>(reviewInfos);
+            }else if(sort.equals("popularity") && order.equals("ascending")){ // 인기 낮은순
+                Collections.sort(reviewInfos, (o1, o2) -> {
+                    if(o1.getRating() > o2.getRating()){
+                        return 1;
+                    }else{
+                        return -1;
+                    }
+                });
+
+                return new BaseResponse<>(reviewInfos);
+            }
+            // default = 최신순
+            Collections.sort(reviewInfos, (o1, o2) -> o2.getReviewIdx() - o1.getReviewIdx());
+
             return new BaseResponse<>(reviewInfos);
         }catch(BaseException exception){
             return new BaseResponse<>((exception.getStatus()));
         }
     }
+
 }
