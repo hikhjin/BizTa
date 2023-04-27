d1eab794b3c2fe424b6019d5b1015029b68a2ce3
ohhyungsuh <ohgudtj_3@naver.com>
2023-04-27 19:03:32 +0900

    [MODIFY] Secret Base64 에러 수정

--- src/main/java/com/api/bizta/config/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.api.bizta.config;
-
-// 공통적으로 사용하는 상수 선언
-public class Constant {
-    public enum SocialLoginType{
-        GOOGLE,
-    }
-}
-
--- src/main/java/com/api/bizta/config/secret/Secret.java
+++ src/main/java/com/api/bizta/config/secret/Secret.java
@@ -1,8 +1,16 @@
 package com.api.bizta.config.secret;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 // TODO: 해당 KEY 값들을 꼭 바꿔서 사용해주세요!
 // TODO: .gitignore에 추가하는거 앚지 마세요!
 public class Secret {
-   // public static String JWT_SECRET_KEY = "UwKYibQQgkW7g-*k.ap9kje-wxBHb9wdXoBT4vnt4P3sJWt-Nul";
-  
+//    public static String JWT_SECRET_KEY = "UwKYibQQgkW7g-*k.ap9kje-wxBHb9wdXoBT4vnt4P3sJWt-Nul";
+    public static String JWT_SECRET_KEY = "EB12221C43B1EA4EAC68619EA9352ED22563A3507B4006B5E84712F974384059";
+
+    public static String getJwtSecretKey() {
+        byte[] decodedKey = JWT_SECRET_KEY.getBytes(StandardCharsets.UTF_8);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(decodedKey);
+    }
 }
