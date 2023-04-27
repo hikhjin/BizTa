569db5fee4eee4e49b1a2703f233a63e40ab4a6d
ohhyungsuh <ohgudtj_3@naver.com>
2023-04-27 19:04:08 +0900

    [ADD] 응답 코드 추가

--- src/main/java/com/api/bizta/config/BaseResponseStatus.java
+++ src/main/java/com/api/bizta/config/BaseResponseStatus.java
@@ -22,14 +22,28 @@ public enum BaseResponseStatus {
     INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
     INVALID_USER_JWT(false,2003,"권한이 없는 유저의 접근입니다."),
 
-    // users
-    USERS_EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해주세요."),
-    USERS_EXISTS_USER_ID(false,2011,"중복된 아이디입니다."),
+//    users
+//    USERS_EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해주세요."),
+//    USERS_EXISTS_USER_ID(false,2011,"중복된 아이디입니다."),
+//    INVALID_AUTH(false, 2005, "유효하지 않은 회원 정보입니다."),
+//    USERS_EXISTS_USER_NAME(false,2012,"중복된 닉네임입니다."),
+//    USERS_EMPTY_USER_NAME(false,2013,"유저 닉네임을 입력해주세요."),
+//
+//    USERS_EMPTY_USER_EMAIL(false, 2014, "유저 이메일 값을 확인해주세요."),
+//    POST_USERS_EXISTS_EMAIL(false,2017,"중복된 이메일입니다."),
 
-    USERS_EXISTS_USER_NAME(false,2012,"중복된 닉네임입니다."),
-    USERS_EMPTY_USER_NAME(false,2013,"유저 닉네임을 입력해주세요."),
 
-
+    // user 회원가입
+    EMPTY_ID(false,2004, "아이디를 입력해주세요."),
+    EXISTS_ID(false,2005,"중복된 아이디입니다."),
+    EMPTY_NICKNAME(false,2006,"닉네임을 입력해주세요."),
+    EXISTS_NICKNAME(false,2007,"중복된 닉네임입니다."),
+    EMPTY_EMAIL(false,2008, "이메일을 입력해주세요."),
+    INVALID_EMAIL(false,2009, "이메일 형식을 확인해주세요."),
+    EXISTS_EMAIL(false,2010,"중복된 이메일입니다."),
+    FAIL_VERIFICATION_EMAIL(false,2011,"이메일 인증이 되지 않았습니다."),
+    EMPTY_PASSWORD(false,2012, "비밀번호를 입력해주세요."),
+    INVALID_PASSWORD(false,2013, "비밀번호 형식을 확인해주세요."),
 
 
 
@@ -39,14 +53,24 @@ public enum BaseResponseStatus {
     // Common
     RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
 
+    FAILED_TO_LOGIN(false,3014,"없는 아이디거나 비밀번호가 틀렸습니다."),
 
     /**
      * 4000 : Database, Server 오류
      */
     DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
     SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다."),
-    REQUESTED_DATA_FAIL_TO_EXIST(false, 4002, "요청한 데이터가 존재하지 않습니다.");
+    REQUESTED_DATA_FAIL_TO_EXIST(false, 4002, "요청한 데이터가 존재하지 않습니다."),
+    PASSWORD_DECRYPTION_ERROR(false, 4012, "비밀번호 복호화에 실패하였습니다."),
+    PASSWORD_ENCRYPTION_ERROR(false, 4011, "비밀번호 암호화에 실패하였습니다.");
 
+//    //[PATCH] /users/{userIdx}
+//    MODIFY_FAIL_USERNAME(false,4014,"유저네임 수정 실패"),
+//
+//    DELETE_USER_FAIL(false, 4008, "유저 정보 삭제에 실패하였습니다."),
+//
+//    PASSWORD_ENCRYPTION_ERROR(false, 4011, "비밀번호 암호화에 실패하였습니다."),
+//    PASSWORD_DECRYPTION_ERROR(false, 4012, "비밀번호 복호화에 실패하였습니다.");
 
 
     // 5000 : 필요시 만들어서 쓰세요
