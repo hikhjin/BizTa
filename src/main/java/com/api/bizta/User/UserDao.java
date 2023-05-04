01d36b2b61ff4491b61f395913128d773fdb3f17
ohhyungsuh <ohgudtj_3@naver.com>
2023-05-04 17:34:41 +0900

    [MODIFY] 코드 일부 수정

--- src/main/java/com/api/bizta/User/UserDao.java
+++ src/main/java/com/api/bizta/User/UserDao.java
@@ -7,8 +7,7 @@ import com.api.bizta.User.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
