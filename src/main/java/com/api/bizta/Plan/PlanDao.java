6d9934fd8345b013acf4cbeba58f1edb82b5f9d1
김현진 <kimhyeonjin@gimhyeonjin-ui-MacBookPro.local>
2023-05-08 20:44:20 +0900

    getPlan 기능 추가

--- src/main/java/com/api/bizta/Plan/PlanDao.java
+++ src/main/java/com/api/bizta/Plan/PlanDao.java
@@ -2,14 +2,15 @@ package com.api.bizta.Plan;
 
 
 import com.api.bizta.Plan.model.*;
-import com.api.bizta.config.BaseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-
-import static com.api.bizta.config.BaseResponseStatus.DATABASE_ERROR;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Repository
 public class PlanDao {
@@ -20,11 +21,11 @@ public class PlanDao {
     public void setDataSource(DataSource dataSource) {this.jdbcTemplate = new JdbcTemplate(dataSource);}
 
     // plan 추가 (interest 제외)
-    public int insertPlanInfo(int userIdx, PostPlanReq postPlanReq) {
+    public int insertPlanInfo(int userIdx, PlanInfo planInfo) {
         String insertPlanQuery = "INSERT INTO Plan(userIdx, country, city, hotel, transport, startDate, endDate, companionCnt) " +
                 "VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
-        Object[] insertPlanParams = new Object[]{userIdx, postPlanReq.getCountry(), postPlanReq.getCity(), postPlanReq.getHotel(),
-        postPlanReq.getTransport(), postPlanReq.getStartDate(), postPlanReq.getEndDate(), postPlanReq.getCompanionCnt()};
+        Object[] insertPlanParams = new Object[]{userIdx, planInfo.getCountry(), planInfo.getCity(), planInfo.getHotel(),
+        planInfo.getTransport(), planInfo.getStartDate(), planInfo.getEndDate(), planInfo.getCompanionCnt()};
         this.jdbcTemplate.update(insertPlanQuery, insertPlanParams);
 
         String lastInsertIdxQuery = "select last_insert_id()"; // 마지막으로 삽입된 Idx 반환
@@ -32,9 +33,9 @@ public class PlanDao {
     }
 
     // plan 추가 (interest만)
-    public int insertPlanInterest(int planIdx, InterestReq interestReq) {
+    public int insertPlanInterest(int planIdx, Interest interest) {
         String insertPlanQuery = "INSERT INTO Interest(planIdx, interest) VALUES(?, ?);";
-        Object[] insertPlanParams = new Object[]{planIdx, interestReq.getInterest()};
+        Object[] insertPlanParams = new Object[]{planIdx, interest.getInterest()};
         return this.jdbcTemplate.update(insertPlanQuery, insertPlanParams);
     }
 
@@ -84,4 +85,67 @@ public class PlanDao {
         Object[] deletePlanParams = new Object[]{planIdx};
         return this.jdbcTemplate.update(deletePlanQuery, deletePlanParams);
     }
+
+    //특정 plan 조회
+    public GetPlanInfo getPlanInfo(int planIdx) {
+
+        String getPlanInfoQuery =
+                "select userIdx, planIdx, country, city, hotel, transport, startDate, endDate, companionCnt " +
+                        "from Plan where planIdx = ? and status = 'active';";
+        String getInterestQuery = "select interest from Interest " +
+                "where planIdx = ? and status = 'active';";
+        try {
+            return this.jdbcTemplate.queryForObject(getPlanInfoQuery,
+                    (rs, rsNum) -> new GetPlanInfo(
+                            rs.getInt("userIdx"),
+                            rs.getInt("planIdx"),
+                            rs.getString("country"),
+                            rs.getString("city"),
+                            rs.getString("hotel"),
+                            rs.getString("transport"),
+                            rs.getString("startDate"),
+                            rs.getString("endDate"),
+                            rs.getInt("companionCnt"),
+                            this.jdbcTemplate.query(getInterestQuery,
+                                    (rk, rkNum) -> new Interest(
+                                            rk.getString("interest")
+                                    ))
+                    ), planIdx);
+        } catch (EmptyResultDataAccessException e) { // 쿼리문에 해당하는 결과가 없을 때
+            return null;
+        }
+
+    }
+
+    /*
+    private RowMapper<GetPlanInfo> planInfoRowMapper(){
+        return new RowMapper<GetPlanInfo>() {
+            @Override
+            public GetPlanInfo mapRow(ResultSet rs, int rowNum) throws SQLException {
+                GetPlanInfo planInfo = new GetPlanInfo();
+                planInfo.setUserIdx(rs.getInt("userIdx"));
+                planInfo.setPlanIdx(rs.getInt("planIdx"));
+                planInfo.setCountry(rs.getString("country"));
+                planInfo.setCity(rs.getString("city"));
+                planInfo.setHotel(rs.getString("hotel"));
+                planInfo.setTransport(rs.getString("transport"));
+                planInfo.setStartDate(rs.getString("startDate"));
+                planInfo.setEndDate(rs.getString("endDate"));
+                return planInfo;
+            }
+        };
+    }
+
+    private RowMapper<Interest> InterestRowMapper(){
+        return new RowMapper<Interest>() {
+            @Override
+            public Interest mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Interest interest = new Interest();
+                interest.setInterest("interest");
+                return interest;
+            }
+        }
+    }
+
+     */
 }
