26d41669a5d6b400bf792ddea091828436fef109
ohhyungsuh <ohgudtj_3@naver.com>
2023-05-26 14:57:44 +0900

    회원가입, 로그인

--- src/main/java/com/api/bizta/User/model/PostLoginRes.java
+++ src/main/java/com/api/bizta/User/model/PostLoginRes.java
@@ -17,12 +17,11 @@ public class PostLoginRes {
     private String nickName;
     private GetTokenRes getTokenRes;
 
-    public PostLoginRes(String id, String password, String email, String nickName, GetTokenRes getTokenRes){
+    public PostLoginRes(String id, String password, String email, String nickName){
         this.id = id;
         this.password = password;
         this.email = email;
         this.nickName = nickName;
-        this.getTokenRes = getTokenRes;
     }
 
     public PostLoginRes(int userIdx, String nickName){
